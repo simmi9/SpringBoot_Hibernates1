5d968927e77b85f9bd7a9615eab5e1b42f15e491
bhawna <bhawna.keswani20@gmail.com>
2021-09-22 23:50:29 -0400

    Using one of the component as primary

--- trainingToday/src/main/java/com/training1/spring/boot/office/trainingToday/BubbleSortAlgorithm.java
+++ trainingToday/src/main/java/com/training1/spring/boot/office/trainingToday/BubbleSortAlgorithm.java
@@ -1,6 +1,8 @@
 package com.training1.spring.boot.office.trainingToday;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	
